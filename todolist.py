cf1a8c188eaaccdc0a4fbd0b9a0f04b9ed0bef29
Uglypr1nces <shpatavdiu4@gmail.com>
2023-11-16 20:47:00 +0100

    updated todolist with a class to save the different objects

--- todolist.py
+++ todolist.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter.font import Font
+import datetime
 
 root = Tk()
 root.geometry("500x300")
@@ -26,7 +27,14 @@ my_list = Listbox(my_frame,
     )
 my_list.pack(side=LEFT,fill=BOTH)
 
-stuff = ["dont go outside", "dunno", "BEAT UP SOME OLD MAN"]
+class thingstodo:
+    def __init__(self, date, end, importance):
+        self.date = date
+        self.end = end
+        self. importance = importance
+        
+stuff = ["example"]
+
 for item in stuff:
     my_list.insert(END, item)
 
@@ -42,12 +50,55 @@ my_entry.pack(pady=20)
 button_frame = Frame(root)
 button_frame.pack(pady=20)
 
+
 def delete_item():
     my_list.delete(ANCHOR)
 
+
+def details():
+    selecteditem = my_list.get(ANCHOR)
+    detailswindow = Toplevel(root)
+    detailswindow.title("Details")
+    detailswindow.geometry("400x200")
+
+    date = datetime.datetime.now()
+    details_frame = LabelFrame(detailswindow, text=f"Details for {selecteditem}")
+    details_frame.pack(pady=10)
+
+    question1 = "wann wollen sie es machen?"
+    question2 = "wie wichtig ist es?"
+
+    mylabel = Label(details_frame, text=f"{question1}")
+    mylabel.pack()
+
+    my_entry1 = Entry(details_frame, font=("Helvetica", 24))
+    my_entry1.pack(pady=20)
+    endline = "nothing"
+    importance = "nothing"
+    
+    def next():
+        endline = my_entry1.get()
+        my_entry1.clipboard_clear()
+        my_entry1.clipboard_append(endline)
+        my_entry1.delete(0, 'end')
+        my_entry1.update()  
+        mylabel.config(text=question2)  
+        importance = my_entry1.get()
+        my_entry1.clipboard_clear()
+        my_entry1.clipboard_append(endline)
+
+    selecteditem = thingstodo(date,endline,importance)
+    print(selecteditem.date)
+    print(selecteditem.end)
+    print(selecteditem.importance)
+    Okbutton = Button(detailswindow,text="ok",command=next)
+    Okbutton.pack()
+
 def add_item():
     my_list.insert(END, my_entry.get())
-    my_entry.delete(0,END)
+    my_entry.delete(0, END)
+    date = datetime.datetime.now()
+
 
 def cross_item():
     my_list.itemconfig(
@@ -64,10 +115,12 @@ delete_button = Button(button_frame,text="Delete smth",command=delete_item)
 add_button = Button(button_frame,text="add smth",command=add_item)
 cross_button = Button(button_frame,text="cross smth",command=cross_item)
 uncross_button = Button(button_frame,text="uncross smth",command=uncross_item)
+details_button = Button(button_frame,text="details",command=details)
 
 delete_button.grid(row=0, column=0)
 add_button.grid(row=0, column=1,padx=20)
 cross_button.grid(row=0, column=2)
 uncross_button.grid(row=0, column=3,padx=20)
+details_button.grid(row=0, column=33,padx=20)
 
 root.mainloop()
